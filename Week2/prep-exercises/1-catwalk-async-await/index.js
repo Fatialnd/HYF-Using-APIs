76c6402e47eba8eef7406cbac7e571885cfe33a6
Fatemeh Alinejad <fatemeh.alinejad.dev@gmail.com>
2024-09-07 10:26:19 +0200

    prep-ex usingAPI week2

--- Week2/prep-exercises/1-catwalk-async-await/index.js
+++ Week2/prep-exercises/1-catwalk-async-await/index.js
@@ -1,30 +1,55 @@
 'use strict';
 
-const STEP_INTERVAL_MS = 50;
 const STEP_SIZE_PX = 10;
+const STEP_INTERVAL_MS = 50;
 const DANCE_TIME_MS = 5000;
 const DANCING_CAT_URL =
   'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
 
 function walk(img, startPos, stopPos) {
-  return new Promise((resolve) => {
-    // Copy over the implementation from last week
+  return new Promise(resolve => {
+    let currentPos = startPos;
+
+    function step() {
+      if (currentPos < stopPos) {
+        currentPos += STEP_SIZE_PX;
+        img.style.left = `${currentPos}px`;
+        setTimeout(step, STEP_INTERVAL_MS);
+      } else {
+        resolve();
+      }
+    }
+
+    step();
   });
 }
 
 function dance(img) {
-  return new Promise((resolve) => {
-    // Copy over the implementation from last week
+  return new Promise(resolve => {
+    const originalSrc = img.src;
+    img.src = DANCING_CAT_URL;
+
+    setTimeout(() => {
+      img.src = originalSrc;
+      resolve();
+    }, DANCE_TIME_MS);
   });
 }
 
-async function catWalk() {
+function catWalk() {
   const img = document.querySelector('img');
   const startPos = -img.width;
   const centerPos = (window.innerWidth - img.width) / 2;
   const stopPos = window.innerWidth;
 
-  // Use async/await syntax to loop the walk and dance functions
+  function performWalkAndDance() {
+    walk(img, startPos, centerPos)
+      .then(() => dance(img))
+      .then(() => walk(img, centerPos, stopPos))
+      .then(performWalkAndDance);
+  }
+
+  performWalkAndDance();
 }
 
 window.addEventListener('load', catWalk);
